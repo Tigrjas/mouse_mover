f5d0477a14f5afd89258ce47f377b161c532141a
Jason Tran <jtran64@wgu.edu>
2024-05-20 19:07:13 -0700

    Seperated actions into functions and called them randomly

--- main.py
+++ main.py
@@ -2,9 +2,31 @@ import pyautogui
 import random
 import time
 
+# Constants
 RUNTIME = time.time() + 10
 
-while time.time() < RUNTIME:
+
+def move_mouse():
     random_x = random.randint(0, 3440)
     random_y = random.randint(0, 1440)
     pyautogui.moveTo(random_x, random_y, 2)
+
+
+def pause_mouse():
+    time.sleep(random.randint(1, 3))
+
+
+def main():
+    # Dictionary of possible actions
+    mydict = {
+        "move": move_mouse,
+        "pause": pause_mouse,
+    }
+
+    # Choosing a random action and running for a specific time
+    while time.time() < RUNTIME:
+        random.choice(list(mydict.values()))()
+
+
+if __name__ == "__main__":
+    main()
